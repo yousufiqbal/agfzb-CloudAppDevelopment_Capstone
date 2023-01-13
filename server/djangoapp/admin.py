446ab5a940688c7f506718464864b4dadbaf955d
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2023-01-13 19:58:32 +0500

    3rd addition

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.StackedInline):
+  model=CarModel
+  extra=3
+
+class CarMakeAdmin(admin.ModelAdmin):
+  inlines = [CarModelInline]
 
 # Register your models here.
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
 
 # CarModelInline class
 
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,21 +4,24 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+  name = models.CharField(max_length=200)
+  description = models.CharField(max_length=500)
+
+  def __str__(self):
+    return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+  TYPES = [ ('Sedan', 'Sedan'), ('SUV', 'SUV'), ('Wagon', 'Wagon'), ]
+  car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+  name = models.CharField(max_length=200)
+  dealer_id = models.IntegerField()
+  type = models.CharField(max_length=100, choices=TYPES)
+  year = models.DateField()
+
+  def __str__(self):
+    return self.name
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
