2771648ba093d017b71d35102e0974563c21a23a
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2023-01-14 18:59:13 +0500

    ..

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS =  ['localhost', 'localhost:8000',  'https://yousufiqbalh-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']  
 
 
 # Application definition
